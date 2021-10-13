9ff2f51cd03cfdf75b67a777dbfa54b0e5be6c18
Naheed Awan <nadsawan@yahoo.com>
2021-10-13 16:08:38 -0400

    commit initial

---
+++ script.js
@@ -0,0 +1,17 @@
+// this is comment
+// let x="5"
+// let y=" 5"
+// console.log(x+y)
+// console.log("11" < "2")
+// console.log(""==false)
+// console.log("END")
+let MyAge = 15
+let ageToDrink =21
+let ageToEnter = 18
+if (MyAge >= 21) {
+    console.log("you can order")
+}else if (MyAge >=18 && MyAge < 21) {
+    console.log("You can come but cannot order")
+}else  {
+    console.log("you cannot enter or order")
+}
